b8a56b747ce4b8c8f352fe44b80ba5eed2568b72
Paul Keith <javelinanddart@gmail.com>
2019-01-19 18:03:15 +0100

    livedisplay: legacymm: Wire it up
    Change-Id: Ic580ceb333c361444d4e009db1a34ccca8fbbbfc

--- legacymm/DisplayModes.cpp
+++ legacymm/DisplayModes.cpp
@@ -14,7 +14,11 @@
  * limitations under the License.
  */
 
+#include <dlfcn.h>
+#include <stdlib.h>
+
 #include "DisplayModes.h"
+#include "PictureAdjustment.h"
 
 namespace vendor {
 namespace lineage {
@@ -22,34 +26,155 @@ namespace livedisplay {
 namespace V2_0 {
 namespace legacymm {
 
+DisplayModes::DisplayModes(void* libHandle) {
+    mLibHandle = libHandle;
+    disp_api_supported =
+        reinterpret_cast<int (*)(int32_t, int32_t)>(dlsym(mLibHandle, "disp_api_supported"));
+    disp_api_get_num_display_modes = reinterpret_cast<int (*)(int32_t, int32_t, int*)>(
+        dlsym(mLibHandle, "disp_api_get_num_display_modes"));
+    disp_api_get_display_modes = reinterpret_cast<int (*)(int32_t, int32_t, void*, int)>(
+        dlsym(mLibHandle, "disp_api_get_display_modes"));
+    disp_api_get_active_display_mode = reinterpret_cast<int (*)(int32_t, int*, uint32_t*)>(
+        dlsym(mLibHandle, "disp_api_get_active_display_mode"));
+    disp_api_set_active_display_mode = reinterpret_cast<int (*)(int32_t, int)>(
+        dlsym(mLibHandle, "disp_api_set_active_display_mode"));
+    disp_api_get_default_display_mode = reinterpret_cast<int (*)(int32_t, int*)>(
+        dlsym(mLibHandle, "disp_api_get_default_display_mode"));
+    disp_api_set_default_display_mode = reinterpret_cast<int (*)(int32_t, int)>(
+        dlsym(mLibHandle, "disp_api_set_default_display_mode"));
+
+    if (isSupported()) {
+        DisplayMode mode = getDefaultDisplayModeInternal();
+        if (mode.id > 0) {
+            setDisplayMode(mode.id, false);
+        }
+    }
+}
+
+bool DisplayModes::isSupported() {
+    int count = 0;
+
+    if (disp_api_get_num_display_modes != nullptr) {
+        if (disp_api_get_num_display_modes(0, 0, &count) == 0) {
+            return count > 0;
+        }
+    }
+
+    return false;
+}
+
+std::vector<DisplayMode> DisplayModes::getDisplayModesInternal() {
+    int count = 0;
+    std::vector<DisplayMode> modes;
+
+    if (disp_api_get_num_display_modes(0, 0, &count) != 0) {
+        return modes;
+    }
+
+    if (disp_api_get_display_modes != nullptr) {
+        mm_disp_mode* tmp = new mm_disp_mode[count];
+        for (int i = 0; i < count; i++) {
+            tmp[i].id = -1;
+            tmp[i].name = new char[128];
+            tmp[i].len = 128;
+        }
+
+        if (disp_api_get_display_modes(0, 0, tmp, count) == 0) {
+            for (int i = 0; i < count; i++) {
+                modes.push_back(DisplayMode{tmp[i].id, std::string(tmp[i].name, tmp[i].len)});
+                delete[] tmp[i].name;
+            }
+        } else {
+            for (int i = 0; i < count; i++) {
+                delete[] tmp[i].name;
+            }
+        }
+
+        delete[] tmp;
+    }
+
+    return modes;
+}
+
+DisplayMode DisplayModes::getDisplayModeById(int32_t id) {
+    std::vector<DisplayMode> modes = getDisplayModesInternal();
+
+    for (const DisplayMode& mode : modes) {
+        if (mode.id == id) {
+            return mode;
+        }
+    }
+
+    return DisplayMode{-1, ""};
+}
+
+DisplayMode DisplayModes::getCurrentDisplayModeInternal() {
+    int id = 0;
+    uint32_t mask = 0;
+
+    if (disp_api_get_active_display_mode != nullptr) {
+        if (disp_api_get_active_display_mode(0, &id, &mask) == 0 && id >= 0) {
+            return getDisplayModeById(id);
+        }
+    }
+
+    return DisplayMode{-1, ""};
+}
+
+DisplayMode DisplayModes::getDefaultDisplayModeInternal() {
+    int id = 0;
+
+    if (disp_api_get_default_display_mode != nullptr) {
+        if (disp_api_get_default_display_mode(0, &id) == 0 && id >= 0) {
+            return getDisplayModeById(id);
+        }
+    }
+
+    return DisplayMode{-1, ""};
+}
+
 // Methods from ::vendor::lineage::livedisplay::V2_0::IDisplayModes follow.
 Return<void> DisplayModes::getDisplayModes(getDisplayModes_cb _hidl_cb) {
-    // TODO implement
+    _hidl_cb(getDisplayModesInternal());
     return Void();
 }
 
 Return<void> DisplayModes::getCurrentDisplayMode(getCurrentDisplayMode_cb _hidl_cb) {
-    // TODO implement
+    _hidl_cb(getCurrentDisplayModeInternal());
     return Void();
 }
 
 Return<void> DisplayModes::getDefaultDisplayMode(getDefaultDisplayMode_cb _hidl_cb) {
-    // TODO implement
+    _hidl_cb(getDefaultDisplayModeInternal());
     return Void();
 }
 
 Return<bool> DisplayModes::setDisplayMode(int32_t modeID, bool makeDefault) {
-    // TODO implement
-    return bool {};
+    DisplayMode currentMode = getCurrentDisplayModeInternal();
+
+    if (currentMode.id >= 0 && currentMode.id == modeID) {
+        return true;
+    }
+
+    DisplayMode mode = getDisplayModeById(modeID);
+    if (mode.id < 0) {
+        return false;
+    }
+
+    if (disp_api_set_active_display_mode == nullptr || disp_api_set_active_display_mode(0, modeID)) {
+        return false;
+    }
+
+    if (makeDefault && (disp_api_set_default_display_mode == nullptr ||
+                        disp_api_set_default_display_mode(0, modeID))) {
+        return false;
+    }
+
+    PictureAdjustment::updateDefaultPictureAdjustment();
+
+    return true;
 }
 
-
-// Methods from ::android::hidl::base::V1_0::IBase follow.
-
-//IDisplayModes* HIDL_FETCH_IDisplayModes(const char* /* name */) {
-    //return new DisplayModes();
-//}
-//
 }  // namespace legacymm
 }  // namespace V2_0
 }  // namespace livedisplay
