cb2103ae5c4c1615511f60bd448dc795c40093d6
Paul Keith <javelinanddart@gmail.com>
2019-01-19 18:03:15 +0100

    livedisplay: legacymm: Wire it up
    Change-Id: Ic580ceb333c361444d4e009db1a34ccca8fbbbfc

--- legacymm/ColorBalance.cpp
+++ legacymm/ColorBalance.cpp
@@ -14,7 +14,11 @@
  * limitations under the License.
  */
 
+#include <dlfcn.h>
+
 #include "ColorBalance.h"
+#include "Constants.h"
+#include "Types.h"
 
 namespace vendor {
 namespace lineage {
@@ -22,29 +26,82 @@ namespace livedisplay {
 namespace V2_0 {
 namespace legacymm {
 
+ColorBalance::ColorBalance(void* libHandle) {
+    mLibHandle = libHandle;
+    disp_api_supported =
+        reinterpret_cast<int (*)(int32_t, int32_t)>(dlsym(mLibHandle, "disp_api_supported"));
+    disp_api_get_color_balance_range = reinterpret_cast<int (*)(int32_t, void*)>(
+        dlsym(mLibHandle, "disp_api_get_color_balance_range"));
+    disp_api_get_color_balance =
+        reinterpret_cast<int (*)(int32_t, int*)>(dlsym(mLibHandle, "disp_api_get_color_balance"));
+    disp_api_set_color_balance =
+        reinterpret_cast<int (*)(int32_t, int)>(dlsym(mLibHandle, "disp_api_set_color_balance"));
+    disp_api_get_num_display_modes = reinterpret_cast<int (*)(int32_t, int32_t, int*)>(
+        dlsym(mLibHandle, "disp_api_get_num_display_modes"));
+}
+
+bool ColorBalance::isSupported() {
+    mm_cb_range range{};
+    int count = 0;
+
+    if (disp_api_supported == nullptr || disp_api_supported(0, COLOR_BALANCE_FEATURE) == 0) {
+        return false;
+    }
+
+    if (disp_api_get_color_balance_range == nullptr ||
+        disp_api_get_color_balance_range(0, &range) != 0) {
+        return false;
+    }
+
+    if (range.max != 0 || range.min != 0) {
+        return false;
+    }
+
+    if (disp_api_supported(0, DISPLAY_MODES_FEATURE) != 0 &&
+        disp_api_get_num_display_modes != nullptr &&
+        disp_api_get_num_display_modes(0, 0, &count) == 0) {
+        return count > 0;
+    }
+
+    return false;
+}
+
 // Methods from ::vendor::lineage::livedisplay::V2_0::IColorBalance follow.
 Return<void> ColorBalance::getColorBalanceRange(getColorBalanceRange_cb _hidl_cb) {
-    // TODO implement
+    Range range{};
+    mm_cb_range r{};
+
+    if (disp_api_get_color_balance_range != nullptr) {
+        if (disp_api_get_color_balance_range(0, &range) == 0) {
+            range.min = r.min;
+            range.max = r.max;
+        }
+    }
+
+    _hidl_cb(range);
     return Void();
 }
 
 Return<int32_t> ColorBalance::getColorBalance() {
-    // TODO implement
-    return int32_t {};
+    int value = 0;
+
+    if (disp_api_get_color_balance != nullptr) {
+        if (disp_api_get_color_balance(0, &value) != 0) {
+            value = 0;
+        }
+    }
+
+    return static_cast<int32_t>(value);
 }
 
 Return<bool> ColorBalance::setColorBalance(int32_t value) {
-    // TODO implement
-    return bool {};
+    if (disp_api_set_color_balance != nullptr) {
+        return disp_api_set_color_balance(0, static_cast<int>(value)) == 0;
+    }
+
+    return false;
 }
 
-
-// Methods from ::android::hidl::base::V1_0::IBase follow.
-
-//IColorBalance* HIDL_FETCH_IColorBalance(const char* /* name */) {
-    //return new ColorBalance();
-//}
-//
 }  // namespace legacymm
 }  // namespace V2_0
 }  // namespace livedisplay
