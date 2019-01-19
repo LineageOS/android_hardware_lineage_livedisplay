9dd8d81f616018c2c4aa5441f012f05c3959b201
Paul Keith <javelinanddart@gmail.com>
2019-01-19 18:03:15 +0100

    livedisplay: legacymm: Wire it up
    Change-Id: Ic580ceb333c361444d4e009db1a34ccca8fbbbfc

--- legacymm/PictureAdjustment.cpp
+++ legacymm/PictureAdjustment.cpp
@@ -14,6 +14,9 @@
  * limitations under the License.
  */
 
+#include <dlfcn.h>
+#include <string.h>
+
 #include "PictureAdjustment.h"
 
 namespace vendor {
@@ -22,54 +25,159 @@ namespace livedisplay {
 namespace V2_0 {
 namespace legacymm {
 
+static sp<PictureAdjustment> sInstance;
+
+PictureAdjustment::PictureAdjustment(void* libHandle) {
+    sInstance = this;
+
+    mLibHandle = libHandle;
+    disp_api_supported =
+        reinterpret_cast<int (*)(int32_t, int32_t)>(dlsym(mLibHandle, "disp_api_supported"));
+    disp_api_get_pa_range =
+        reinterpret_cast<int (*)(int32_t, void*)>(dlsym(mLibHandle, "disp_api_get_pa_range"));
+    disp_api_get_pa_config =
+        reinterpret_cast<int (*)(int32_t, void*)>(dlsym(mLibHandle, "disp_api_get_pa_config"));
+    disp_api_set_pa_config =
+        reinterpret_cast<int (*)(int32_t, void*)>(dlsym(mLibHandle, "disp_api_set_pa_config"));
+    mDefaultPictureAdjustment = HSIC{};
+}
+
+bool PictureAdjustment::isSupported() {
+    mm_pa_range r{};
+
+    if (disp_api_supported == nullptr || disp_api_supported(0, PICTURE_ADJUSTMENT_FEATURE) == 0) {
+        return false;
+    }
+
+    if (disp_api_get_pa_range == nullptr || disp_api_get_pa_range(0, &r) != 0) {
+        return false;
+    }
+
+    return !(r.max.hue == 0 || r.min.hue == 0 || r.max.saturation == 0.f ||
+             r.min.saturation == 0.f || r.max.intensity == 0.f || r.min.intensity == 0.f ||
+             r.max.contrast == 0.f || r.min.contrast == 0.f);
+}
+
+HSIC PictureAdjustment::getPictureAdjustmentInternal() {
+    mm_pa_config config{};
+
+    if (disp_api_get_pa_config != nullptr) {
+        if (disp_api_get_pa_config(0, &config) == 0) {
+            return HSIC{
+                static_cast<float>(config.data.hue), static_cast<float>(config.data.saturation),
+                static_cast<float>(config.data.intensity), static_cast<float>(config.data.contrast),
+                static_cast<float>(config.data.saturationThreshold)};
+        }
+    }
+
+    return HSIC{};
+}
+
+void PictureAdjustment::updateDefaultPictureAdjustment() {
+    if (sInstance != nullptr) {
+        sInstance->mDefaultPictureAdjustment = sInstance->getPictureAdjustmentInternal();
+    }
+}
+
 // Methods from ::vendor::lineage::livedisplay::V2_0::IPictureAdjustment follow.
 Return<void> PictureAdjustment::getHueRange(getHueRange_cb _hidl_cb) {
-    // TODO implement
+    FloatRange range{};
+    mm_pa_range r{};
+
+    if (disp_api_get_pa_range != nullptr) {
+        if (disp_api_get_pa_range(0, &r) == 0) {
+            range.max = r.max.hue;
+            range.min = r.min.hue;
+        }
+    }
+
+    _hidl_cb(range);
     return Void();
 }
 
 Return<void> PictureAdjustment::getSaturationRange(getSaturationRange_cb _hidl_cb) {
-    // TODO implement
+    FloatRange range{};
+    mm_pa_range r{};
+
+    if (disp_api_get_pa_range != nullptr) {
+        if (disp_api_get_pa_range(0, &r) == 0) {
+            range.max = r.max.saturation;
+            range.min = r.min.saturation;
+        }
+    }
+
+    _hidl_cb(range);
     return Void();
 }
 
 Return<void> PictureAdjustment::getIntensityRange(getIntensityRange_cb _hidl_cb) {
-    // TODO implement
+    FloatRange range{};
+    mm_pa_range r{};
+
+    if (disp_api_get_pa_range != nullptr) {
+        if (disp_api_get_pa_range(0, &r) == 0) {
+            range.max = r.max.intensity;
+            range.min = r.min.intensity;
+        }
+    }
+
+    _hidl_cb(range);
     return Void();
 }
 
 Return<void> PictureAdjustment::getContrastRange(getContrastRange_cb _hidl_cb) {
-    // TODO implement
+    FloatRange range{};
+    mm_pa_range r{};
+
+    if (disp_api_get_pa_range != nullptr) {
+        if (disp_api_get_pa_range(0, &r) == 0) {
+            range.max = r.max.contrast;
+            range.min = r.min.contrast;
+        }
+    }
+
+    _hidl_cb(range);
     return Void();
 }
 
 Return<void> PictureAdjustment::getSaturationThresholdRange(getSaturationThresholdRange_cb _hidl_cb) {
-    // TODO implement
+    FloatRange range{};
+    mm_pa_range r{};
+
+    if (disp_api_get_pa_range != nullptr) {
+        if (disp_api_get_pa_range(0, &r) == 0) {
+            range.max = r.max.saturationThreshold;
+            range.min = r.min.saturationThreshold;
+        }
+    }
+
+    _hidl_cb(range);
     return Void();
 }
 
 Return<void> PictureAdjustment::getPictureAdjustment(getPictureAdjustment_cb _hidl_cb) {
-    // TODO implement
+    _hidl_cb(getPictureAdjustmentInternal());
     return Void();
 }
 
 Return<void> PictureAdjustment::getDefaultPictureAdjustment(getDefaultPictureAdjustment_cb _hidl_cb) {
-    // TODO implement
+    _hidl_cb(mDefaultPictureAdjustment);
     return Void();
 }
 
-Return<bool> PictureAdjustment::setPictureAdjustment(const ::vendor::lineage::livedisplay::V2_0::HSIC& hsic) {
-    // TODO implement
-    return bool {};
+Return<bool> PictureAdjustment::setPictureAdjustment(
+    const ::vendor::lineage::livedisplay::V2_0::HSIC& hsic) {
+    mm_pa_config config = {0xF,
+                           {static_cast<int>(hsic.hue), static_cast<int>(hsic.saturation),
+                            static_cast<int>(hsic.intensity), static_cast<int>(hsic.contrast),
+                            static_cast<int>(hsic.saturationThreshold)}};
+    if (disp_api_set_pa_config != nullptr) {
+        return disp_api_set_pa_config(0, &config);
+    }
+
+    return false;
 }
 
-
-// Methods from ::android::hidl::base::V1_0::IBase follow.
-
-//IPictureAdjustment* HIDL_FETCH_IPictureAdjustment(const char* /* name */) {
-    //return new PictureAdjustment();
-//}
-//
 }  // namespace legacymm
 }  // namespace V2_0
 }  // namespace livedisplay
