0d26b6b25acd7fc0ac26836eb23cfd36c2ce9e8e
dianlujitao <dianlujitao@lineageos.org>
2019-02-04 10:34:27 +0800

    livedisplay: Nuke color balance
    * The isSupported() method always returns false here, and
      there are currently no plans to fix it at this time
    
    Change-Id: I45cec0542219b42009f7e4f2a1ab2a1fe0d06ede

--- legacymm/service.cpp
+++ legacymm/service.cpp
@@ -22,7 +22,6 @@
 #include <binder/ProcessState.h>
 #include <hidl/HidlTransportSupport.h>
 
-#include "ColorBalance.h"
 #include "DisplayModes.h"
 #include "PictureAdjustment.h"
 
@@ -34,10 +33,8 @@ using android::status_t;
 using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
-using ::vendor::lineage::livedisplay::V2_0::IColorBalance;
 using ::vendor::lineage::livedisplay::V2_0::IDisplayModes;
 using ::vendor::lineage::livedisplay::V2_0::IPictureAdjustment;
-using ::vendor::lineage::livedisplay::V2_0::legacymm::ColorBalance;
 using ::vendor::lineage::livedisplay::V2_0::legacymm::DisplayModes;
 using ::vendor::lineage::livedisplay::V2_0::legacymm::PictureAdjustment;
 
@@ -47,7 +44,6 @@ int main() {
     int (*disp_api_init)(int32_t) = nullptr;
 
     // HIDL frontend
-    sp<ColorBalance> cb;
     sp<DisplayModes> dm;
     sp<PictureAdjustment> pa;
 
@@ -73,12 +69,6 @@ int main() {
         goto shutdown;
     }
 
-    cb = new ColorBalance(libHandle);
-    if (cb == nullptr) {
-        LOG(ERROR) << "Can not create an instance of LiveDisplay HAL ColorBalance Iface, exiting.";
-        goto shutdown;
-    }
-
     dm = new DisplayModes(libHandle);
     if (dm == nullptr) {
         LOG(ERROR) << "Can not create an instance of LiveDisplay HAL DisplayModes Iface, exiting.";
@@ -92,21 +82,12 @@ int main() {
         goto shutdown;
     }
 
-    if (!cb->isSupported() && !dm->isSupported() && !pa->isSupported()) {
+    if (!dm->isSupported() && !pa->isSupported()) {
         // Backend isn't ready yet, so restart and try again
         goto shutdown;
     }
 
     configureRpcThreadpool(1, true /*callerWillJoin*/);
-
-    if (cb->isSupported()) {
-        status = cb->registerAsService();
-        if (status != OK) {
-            LOG(ERROR) << "Could not register service for LiveDisplay HAL ColorBalance Iface ("
-                       << status << ")";
-            goto shutdown;
-        }
-    }
 
     if (dm->isSupported()) {
         status = dm->registerAsService();
