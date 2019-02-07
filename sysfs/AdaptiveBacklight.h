8d13803c5eaa56097b6b80f2568f198c6bbc5fb1
Paul Keith <javelinanddart@gmail.com>
2019-02-07 09:57:05 -0600

    livedisplay: sysfs: Wire it up
    Change-Id: I8e8e222f94bcf86d4ded3d8dc57cc6c70ee7634a

--- sysfs/AdaptiveBacklight.h
+++ sysfs/AdaptiveBacklight.h
@@ -18,8 +18,6 @@
 #define VENDOR_LINEAGE_LIVEDISPLAY_V2_0_ADAPTIVEBACKLIGHT_H
 
 #include <vendor/lineage/livedisplay/2.0/IAdaptiveBacklight.h>
-#include <hidl/MQDescriptor.h>
-#include <hidl/Status.h>
 
 namespace vendor {
 namespace lineage {
@@ -27,25 +25,25 @@ namespace livedisplay {
 namespace V2_0 {
 namespace sysfs {
 
-using ::android::hardware::hidl_array;
-using ::android::hardware::hidl_memory;
-using ::android::hardware::hidl_string;
-using ::android::hardware::hidl_vec;
 using ::android::hardware::Return;
 using ::android::hardware::Void;
-using ::android::sp;
 
-struct AdaptiveBacklight : public IAdaptiveBacklight {
+#define FOSS_PROPERTY "ro.vendor.display.foss"
+
+#define FILE_ACL "/sys/class/graphics/fb0/acl"
+#define FILE_CABC "/sys/class/graphics/fb0/cabc"
+
+class AdaptiveBacklight : public IAdaptiveBacklight {
+   public:
+    bool isSupported();
+
     // Methods from ::vendor::lineage::livedisplay::V2_0::IAdaptiveBacklight follow.
     Return<bool> isEnabled() override;
     Return<bool> setEnabled(bool enabled) override;
 
-    // Methods from ::android::hidl::base::V1_0::IBase follow.
-
+   private:
+    std::string mFile;
 };
-
-// FIXME: most likely delete, this is only for passthrough implementations
-// extern "C" IAdaptiveBacklight* HIDL_FETCH_IAdaptiveBacklight(const char* name);
 
 }  // namespace sysfs
 }  // namespace V2_0
--- sysfs/ColorEnhancement.h
+++ sysfs/ColorEnhancement.h
@@ -18,8 +18,6 @@
 #define VENDOR_LINEAGE_LIVEDISPLAY_V2_0_COLORENHANCEMENT_H
 
 #include <vendor/lineage/livedisplay/2.0/IColorEnhancement.h>
-#include <hidl/MQDescriptor.h>
-#include <hidl/Status.h>
 
 namespace vendor {
 namespace lineage {
@@ -27,25 +25,19 @@ namespace livedisplay {
 namespace V2_0 {
 namespace sysfs {
 
-using ::android::hardware::hidl_array;
-using ::android::hardware::hidl_memory;
-using ::android::hardware::hidl_string;
-using ::android::hardware::hidl_vec;
 using ::android::hardware::Return;
 using ::android::hardware::Void;
-using ::android::sp;
 
-struct ColorEnhancement : public IColorEnhancement {
+#define FILE_CE "/sys/class/graphics/fb0/color_enhance"
+
+class ColorEnhancement : public IColorEnhancement {
+   public:
+    bool isSupported();
+
     // Methods from ::vendor::lineage::livedisplay::V2_0::IColorEnhancement follow.
     Return<bool> isEnabled() override;
     Return<bool> setEnabled(bool enabled) override;
-
-    // Methods from ::android::hidl::base::V1_0::IBase follow.
-
 };
-
-// FIXME: most likely delete, this is only for passthrough implementations
-// extern "C" IColorEnhancement* HIDL_FETCH_IColorEnhancement(const char* name);
 
 }  // namespace sysfs
 }  // namespace V2_0
