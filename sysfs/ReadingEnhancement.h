24865b89aecbd3d06a0d6ee1bcb7392aea9e97aa
dianlujitao <dianlujitao@lineageos.org>
2020-06-24 23:35:18 +0800

    livedisplay: sysfs: Move string literals to anonymous namespace
    Change-Id: Ie0ee6dc0085623b032bd07cdec965a2c83c198e9

--- sysfs/ReadingEnhancement.h
+++ sysfs/ReadingEnhancement.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The LineageOS Project
+ * Copyright (C) 2019-2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,8 +27,6 @@ namespace sysfs {
 
 using ::android::hardware::Return;
 using ::android::hardware::Void;
-
-#define FILE_RE "/sys/class/graphics/fb0/reading_mode"
 
 class ReadingEnhancement : public IReadingEnhancement {
   public:
