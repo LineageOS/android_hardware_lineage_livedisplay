e39e6330cce2054d85bdf61b7fee1742e8055cf5
dianlujitao <dianlujitao@lineageos.org>
2020-06-24 17:41:07 +0800

    livedisplay: sdm: Follow naming rules of Google C++ Style Guide
    Change-Id: I8a1a5695a3682906a53af4449510941339c3bcd3

--- sdm/Types.h
+++ sdm/Types.h
@@ -25,31 +25,31 @@ namespace livedisplay {
 namespace V2_0 {
 namespace sdm {
 
-struct sdm_feature_version {
+struct SdmFeatureVersion {
     uint8_t x, y;
     uint16_t z;
 };
 
-struct sdm_disp_mode {
+struct SdmDispMode {
     int32_t id;
     int32_t type;
     int32_t len;
     char* name;
-    sdm_disp_mode() : id(-1), type(0), len(128), name(new char[128]()) {}
-    ~sdm_disp_mode() { delete[] name; }
-    sdm_disp_mode(const sdm_disp_mode& other)
+    SdmDispMode() : id(-1), type(0), len(128), name(new char[128]()) {}
+    ~SdmDispMode() { delete[] name; }
+    SdmDispMode(const SdmDispMode& other)
         : id(other.id), type(other.type), len(other.len), name(new char[128]()) {
         size_t sz = std::min(strlen(other.name), size_t{127});
         std::copy_n(other.name, sz, name);
         name[sz] = '\0';
     }
-    sdm_disp_mode(sdm_disp_mode&& other) noexcept
+    SdmDispMode(SdmDispMode&& other) noexcept
         : id(std::exchange(other.id, -1)),
           type(std::exchange(other.type, 0)),
           len(std::exchange(other.len, 0)),
           name(std::exchange(other.name, nullptr)) {}
-    sdm_disp_mode& operator=(const sdm_disp_mode& other) { return *this = sdm_disp_mode(other); }
-    sdm_disp_mode& operator=(sdm_disp_mode&& other) noexcept {
+    SdmDispMode& operator=(const SdmDispMode& other) { return *this = SdmDispMode(other); }
+    SdmDispMode& operator=(SdmDispMode&& other) noexcept {
         std::swap(id, other.id);
         std::swap(type, other.type);
         std::swap(len, other.len);
@@ -58,38 +58,38 @@ struct sdm_disp_mode {
     }
 };
 
-struct hsic_data {
+struct HsicData {
     int32_t hue;
     float saturation;
     float intensity;
     float contrast;
-    float saturationThreshold;
+    float saturation_threshold;
 };
 
-struct hsic_config {
+struct HsicConfig {
     uint32_t unused;
-    hsic_data data;
+    HsicData data;
 };
 
-struct hsic_int_range {
+struct HsicIntRange {
     int32_t max;
     int32_t min;
     uint32_t step;
 };
 
-struct hsic_float_range {
+struct HsicFloatRange {
     float max;
     float min;
     float step;
 };
 
-struct hsic_ranges {
+struct HsicRanges {
     uint32_t unused;
-    struct hsic_int_range hue;
-    struct hsic_float_range saturation;
-    struct hsic_float_range intensity;
-    struct hsic_float_range contrast;
-    struct hsic_float_range saturationThreshold;
+    struct HsicIntRange hue;
+    struct HsicFloatRange saturation;
+    struct HsicFloatRange intensity;
+    struct HsicFloatRange contrast;
+    struct HsicFloatRange saturation_threshold;
 };
 
 }  // namespace sdm
