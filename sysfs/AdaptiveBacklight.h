c04f39bfbad39a18ba38f1abe2fc43e19ad97710
dianlujitao <dianlujitao@lineageos.org>
2020-06-24 23:17:30 +0800

    livedisplay: sysfs: Follow naming rules of Google C++ Style Guide
    Change-Id: I447adfcdad4a79bd5f68a25f7fc4aa54f866d6ad

--- sysfs/AdaptiveBacklight.h
+++ sysfs/AdaptiveBacklight.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The LineageOS Project
+ * Copyright (C) 2019-2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,7 +42,7 @@ class AdaptiveBacklight : public IAdaptiveBacklight {
     Return<bool> setEnabled(bool enabled) override;
 
   private:
-    std::string mFile;
+    std::string file_;
 };
 
 }  // namespace sysfs
