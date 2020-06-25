dd815b5ffceb77c59d46176d2c3ca893ca93c71f
dianlujitao <dianlujitao@lineageos.org>
2020-06-25 10:14:28 +0800

    livedisplay: sysfs: Use POSIX api to check file access
    Change-Id: I89da41222ac5f41b8b63a102efe140853989f6b1

--- sysfs/ReadingEnhancement.cpp
+++ sysfs/ReadingEnhancement.cpp
@@ -19,8 +19,6 @@
 #include <android-base/file.h>
 #include <android-base/strings.h>
 
-#include <fstream>
-
 namespace {
 constexpr const char* kFileRe = "/sys/class/graphics/fb0/reading_mode";
 };  // anonymous namespace
@@ -36,9 +34,7 @@ namespace V2_0 {
 namespace sysfs {
 
 bool ReadingEnhancement::isSupported() {
-    std::fstream re(kFileRe, re.in | re.out);
-
-    return re.good();
+    return !access(kFileRe, R_OK | W_OK);
 }
 
 // Methods from ::vendor::lineage::livedisplay::V2_0::IReadingEnhancement follow.
